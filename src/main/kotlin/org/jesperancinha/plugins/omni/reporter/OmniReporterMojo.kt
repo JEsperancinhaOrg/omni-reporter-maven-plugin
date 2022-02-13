c40f2024735d2d1bb92cf29b2051ed88af928f35
Joao Esperancinha <jofisaes@gmail.com>
2022-02-13 22:27:46 +0100

    🌿 Filters of Scala jars and reader for config json

--- src/main/kotlin/org/jesperancinha/plugins/omni/reporter/OmniReporterMojo.kt
+++ src/main/kotlin/org/jesperancinha/plugins/omni/reporter/OmniReporterMojo.kt
@@ -4,7 +4,6 @@ import org.apache.maven.plugin.AbstractMojo
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
-import org.slf4j.LoggerFactory
 import java.io.File
 
 private val List<MavenProject>.toOmniProjects: List<OmniProject>
